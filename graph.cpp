59f54cfb353f2e020990f46d2b931df0b6934ec5
cwyers5 <caw138@zips.uakron.edu>
2017-04-26 14:59:31 -0400

    deleted useless comments

--- graph.cpp
+++ graph.cpp
@@ -133,59 +133,8 @@ std::cout << "HERE" << std::endl;
 		std::cout << names[j] << "\n";
 	}
 	std::cout << "\n";
-	//std::cout << stringvalues << "\n\n\n" << std::endl;
-	  //fill array with numbers in string
-	//  int values[width][height];
-	/*
-	  int currentspace = 0; //current spot on string, starting point for next number
-	  int nextspace; // location of next space, which seperates the numbers
-		for (int h = 0; h < height; h++){
-		for (int w = 0; w < width; w++){
-			int nextspace = stringvalues.find(" ", currentspace); //fill array with stringvalues
-			values[w][h] = std::stoi(stringvalues.substr(currentspace, currentspace - nextspace));
-			currentspace = nextspace + 1;
-		}
-		}
 
-	//print values to ensure they were read correctly
-	for (int h = 0; h < height; h++){
-		for (int w = 0; w < width; w++){
-			std::cout << values[w][h] << " ";
-		}
-		std::cout << std::endl;
-	}
 
-	  //this is a useful reference to make sure everything was read correctly
-	  std::cout << "s contains: " << s << std::endl;
-	  std::cout << "comment contains: " << comment << std::endl;
-	  std::cout << "width = " << width << std::endl;
-	  std::cout << "height = " << height << std::endl;
-	  std::cout << "IMAGE HAS BEEN PROCESSED" << std::endl;
-		std::string processedstring = "";
-		for (int h = 0; h < height; h++){
-		  for (int w = 0; w < width; w++){
-					if (values[w][h] != -1)
-					processedstring += std::to_string(values[w][h]) + " ";
-			//std::cout << values[w][h] << " ";
-		  }
-				processedstring += "\n";
-				//std::cout << std::endl;
-		}
-			//recalculate dimensions with new width and height, so that they are written to file correctly
-			dimensions = std::to_string(width) + " " + std::to_string(height);
-			//combine all strings back together to write to the file
-			s += "\n" + comment + "\n" + dimensions + "\n" + maxcolor + "\n" + processedstring;
-
-		//export file here
-	  int stringsize = file.size()- 4;
-	  std::string filenam = file.substr(0,stringsize);
-	  filenam += "_processed.pgm";
-	  const char * processed = s.c_str();
-	  std::streampos fsize =  s.size();
-	  std::ofstream myfile9 (filenam.c_str());
-	  myfile9.write (processed,fsize);
-	  myfile9.close();
-	*/
 	//std::cin >> file;
 	return 0;
 }
